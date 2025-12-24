4186070e331733fe8cbbefb105a2053d132f7203
Gustavo Henrique <gustavo.tin2@gmail.com>
2025-12-24 13:15:25 -0300

    feat(admin): add admin authentication to the UI

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,6 +1,13 @@
 const navbarToggler = document.querySelector(".nav__toggler");
 const navbarExpand = document.querySelector(".nav__expand");
+const adminButton = document.getElementById("admin-button");
+const adminForm = document.getElementById("admin-form");
 
 navbarToggler.addEventListener("click", () => {
     navbarExpand.classList.toggle("nav__expand--active");
+    adminForm.classList.add("hidden");
 });
+
+adminButton.addEventListener("click", () => {
+    adminForm.classList.toggle("hidden");
+})
